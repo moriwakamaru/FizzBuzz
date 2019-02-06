4dcc658c3b9527714d91b0be654a6e47b4b62a00
waka <wa_morita@route-zero.com>
2019-02-06 21:45:40 +0900

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -6,6 +6,7 @@ public class FizzBuzz
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
+		System.out.print("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
 		int number =sc.nextInt();
 		
 		for(int i=1;i<=number;i++)
